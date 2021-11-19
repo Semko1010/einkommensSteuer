f835f10f5cd228f5b84b55f5f5f4a87f091e5c29
Semko1010 <semir01020@gmail.com>
2021-11-19 17:30:16 +0100

    v1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,31 @@ let einkommensSteuer = document.getElementById("einkommensSteuer");
 let kirchensteuer = document.getElementById("kirchensteuer");
 let gesamtbelastung = document.getElementById("gesamtbelastung");
 
-let fall1;
+
 const berechnen = function() {
     if (jahr.value == 2021 && input.value <= 9744) {
         einkommensSteuer.innerHTML = 0 + " €";
         kirchensteuer.innerHTML = 0 + " €";
         gesamtbelastung.innerHTML = 0 + " €";
-        if (kirchensteuer.value == 9) {}
-    } else if (jahr.value == 2021 && input.value >= 9745 && input.value <= 14753) {
+    }
+    if (jahr.value == 2021 && input.value >= 9745 && input.value <= 14753) {
         let case1 = (input.value - 9744) / 10000
         let case2 = (995.21 * case1 + 1400) * case1;
         einkommensSteuer.innerHTML = case2.toFixed(2) + " €";
-        kirchensteuer.innerHTML = 0 + " €";
         gesamtbelastung.innerHTML = case2.toFixed(2) + " €";
+
+
+        if (kirchenSteuer.value == 8) {
+            let kirch = case2 * 1.08;
+            kirchensteuer.innerHTML = (kirch - case2).toFixed(2) + " €";
+            einkommensSteuer.innerHTML = case2.toFixed(2) + " €";
+            gesamtbelastung.innerHTML = (case2 * 1.08).toFixed(2) + " €";
+        }
+        if (kirchenSteuer.value == 9) {
+            let kirch = case2 * 1.09;
+            kirchensteuer.innerHTML = (kirch - case2).toFixed(2) + " €";
+            einkommensSteuer.innerHTML = case2.toFixed(2) + " €";
+            gesamtbelastung.innerHTML = (case2 * 1.09).toFixed(2) + " €";
+        }
     }
 }
